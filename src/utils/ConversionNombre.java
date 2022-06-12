eccc7800a08ccdae0c4ccc345577563e9cd2521b
boudimohand <boudi_mohand@yahoo.fr>
2022-06-12 18:41:50 +0200

    Creation d'une classe ConversionNombre

---
+++ src/utils/ConversionNombre.java
@@ -0,0 +1,14 @@
+package utils;
+
+public class ConversionNombre {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String chaine= "12";
+		int a = Integer.parseInt(chaine);
+		int b = 15;
+		System.out.println("Le max de a et b est : "+Integer.max(a, b));
+
+	}
+
+}
