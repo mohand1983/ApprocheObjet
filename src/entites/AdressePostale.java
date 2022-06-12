c3971484e098846a725e57b9675aa85e626acf81
boudimohand <boudi_mohand@yahoo.fr>
2022-06-12 17:56:00 +0200

    Création des classes personne, testAdresse et adresse postale

---
+++ src/entites/AdressePostale.java
@@ -0,0 +1,18 @@
+package entites;
+
+public class AdressePostale {
+	// les attributs
+	
+	public int numeroDeRue;
+	public String libelleDeLaRue;
+	public String codePostale;
+	public String ville;
+	// le constructeur
+	
+	public AdressePostale() {
+	
+	}
+	
+	
+
+}
---
+++ src/entites2/Personne.java
@@ -0,0 +1,15 @@
+package entites2;
+
+public class Personne {
+
+		// les attributs 
+	public String nom;
+	public String prenom;
+	public String AdressePostale;
+		// le constructeur
+	public Personne() {
+	
+	}
+	
+
+}
